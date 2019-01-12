4baaf629d2b5bc54d4f6f0dc08fce04f201e8c4d
paschalidi <paschalidi@outlook.com>
2019-01-12 23:48:22 +0100

    jest-styled-components

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,6 @@
 import { configure } from '@storybook/react';
 
-const req = require.context('../src/stories', true, /.story.js$/); // <- import all the stories at once
+const req = require.context('../stories', true, /.story.jsx$/); // <- import all the stories at once
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
--- stories/Textarea.story.jsx
+++ stories/Textarea.story.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import Textarea from "../../src/components/Textarea";
+import Textarea from "../src/Textarea/index";
 
 storiesOf("Textarea", module)
   .add("default", () => <Textarea label="Default" />)
